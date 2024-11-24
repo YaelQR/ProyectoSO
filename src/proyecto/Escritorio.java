d6dfc73598de60306178cc8064a695b678fa8506
Yael Quintanilla <yael.quintanilla.rmz2@outlook.com>
2024-11-23 20:36:01 -0600

    Se agregaron los codigos para la deteccion de interbloqueo

--- src/proyecto/Escritorio.java
+++ src/proyecto/Escritorio.java
@@ -14,10 +14,148 @@ public class Escritorio {
     private int []disponibles;
     private int [][]solicitud;
     private int [][]asignacion;
-    
-    
-    
+    private Proceso []listos;
 
+    public Escritorio() {
+    }
+
+    public Escritorio(int[] recursos, int[] disponibles, int[][] solicitud, int[][] asignacion, Proceso[] listos) {
+        this.recursos = recursos;
+        this.disponibles = disponibles;
+        this.solicitud = solicitud;
+        this.asignacion = asignacion;
+        this.listos = listos;
+    }
+    
+    //algoritmos para deteccion
+    
+    public void deteccion(){
+        
+        boolean []procesosSA=new boolean[solicitud.length];
+        int proceso;
+        boolean safe=true;
+        
+        //Primer paso
+        procesosSA=marcar(procesosSA);
+        
+        //Segundo paso
+        int []T=disponibles;
+        //System.out.println("---------------------"); //Eliminar
+        //Tercer paso
+        do{
+            proceso=encontrarProceso(procesosSA,T);
+            //System.out.println("Proceso "+proceso+" se saco"); //Eliminar
+            if(proceso==-1){
+                System.out.println("Interbloqueo Detectado");
+                safe=false;
+            }  
+            else{
+                procesosSA[proceso]=true;
+                T=agregarR(T,proceso);
+            }
+        }while(safe);
+        
+        //System.out.println("---------------------"); //Eliminar
+        
+        if(!safe){
+            encontrarInterbloqueos(procesosSA);
+        }
+        
+    }
+    
+    public boolean []marcar(boolean []procesosSA){
+        boolean flag;
+        
+        for(int i=0; i<procesosSA.length; i++){
+            procesosSA[i]=false;
+        }
+        
+        for(int i=0; i<asignacion.length; i++){
+            flag=true;
+            for(int j=0; j<asignacion[i].length; j++){
+                if(asignacion[i][j]>0){
+                    flag=false;
+                    break;
+                }    
+            }
+            procesosSA[i]=flag;
+            
+            //String res = (flag)? "Proceso "+i+" se marco":"Proceso "+i+" no se marco"; //Eliminar
+            //System.out.println(res); //Eliminar
+        }
+        return procesosSA;
+    }
+    
+    public int encontrarProceso(boolean []procesosSA, int []T){
+        boolean valido=false;
+        for(int i=0; i<solicitud.length; i++){
+            
+            if(procesosSA[i]==false){
+                //Eliminar
+                /*System.out.println("Vector T");
+                imprime(T);
+                System.out.println("Proceso "+i);
+                imprime(solicitud[i]);*/
+                
+                valido=true;
+                for(int j=0; j<solicitud[i].length; j++){
+                    if(solicitud[i][j] > T[j]){
+                        valido=false;
+                    }
+                }
+                //System.out.println(valido+"\n"); //Eliminar
+            }
+            
+            if(valido)
+                return i;
+        }
+        
+        return -1;
+    } 
+    
+    public int []agregarR(int []T, int proceso){
+        
+        for(int j=0; j<asignacion[proceso].length; j++){
+            T[j]+=asignacion[proceso][j];
+        }
+        
+        return T;
+    }
+    
+    public void encontrarInterbloqueos(boolean []procesosSA){
+        
+        for(int i=0; i<procesosSA.length; i++){
+            if(!procesosSA[i])
+                System.out.println("Proceso "+i+" en interbloqueo");
+            else
+                System.out.println("Proceso "+i+" no esta en interbloqueo");
+        }
+        
+    }
+    
+    public void imprime(int []vector){
+        for(int i=0; i<vector.length; i++){
+            System.out.print(vector[i]+", ");
+        }
+        System.out.println("");
+    }
+    
+    //Algoritmos para prediccion
+    
+    
+    
+    //Algoritmos para recuperacion
+    
+    //Recuperar recursos de un proceso
+    public int []recuperaRecursos(int p, int []recursos){
+        
+        
+        
+        
+        return recursos;
+    }
+    
+    
     /**
      * @return the recursos
      */
@@ -72,6 +210,20 @@ public class Escritorio {
      */
     public void setAsignacion(int[][] asignacion) {
         this.asignacion = asignacion;
+    }
+
+    /**
+     * @return the listos
+     */
+    public Proceso[] getListos() {
+        return listos;
+    }
+
+    /**
+     * @param listos the listos to set
+     */
+    public void setListos(Proceso[] listos) {
+        this.listos = listos;
     }
     
     
